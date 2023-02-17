d40ef0b3025c4c153844513d1d9726d9e25ff6e6
20emigdal <20emigdal@castilleja.org>
2023-02-16 20:22:01 -0600

    last bit done

--- birthweight_smoking.R
+++ birthweight_smoking.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(readxl)
 birthweight_smoking <- read_excel("birthweight_smoking.xlsx")
 
-smokerReg <- lm(birthweight ~ 1 + smoker, data = birthweight_smoking)
+smokerReg <- lm(birthweight ~ 1+ smoker, data = birthweight_smoking)
 summary ( smokerReg )
 
 smokerAlcoholNprevistReg <- lm(birthweight ~ 1 + smoker + alcohol + nprevist, data = birthweight_smoking)
@@ -18,3 +18,6 @@ library(lmtest)
 
 coeftest (smokerAlcoholNprevistReg , vcov = vcovHC (smokerAlcoholNprevistReg , type = "HC1"))
 
+Y <- resid(lm(birthweight~alcohol+nprevist, birthweight_smoking))
+X <- resid(lm(smoker~alcohol+nprevist, birthweight_smoking))
+summary(lm(Y~X))
