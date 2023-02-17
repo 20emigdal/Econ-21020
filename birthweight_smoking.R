7e1b52bd970d9911f4ba3674fc8a0bfc67aa4c13
20emigdal <20emigdal@castilleja.org>
2023-02-16 20:34:02 -0600

    okay done

--- birthweight_smoking.R
+++ birthweight_smoking.R
@@ -2,10 +2,10 @@ library(dplyr)
 library(readxl)
 birthweight_smoking <- read_excel("birthweight_smoking.xlsx")
 
-smokerReg <- lm(birthweight ~ 1+ smoker, data = birthweight_smoking)
+smokerReg <- lm(birthweight ~ smoker, data = birthweight_smoking)
 summary ( smokerReg )
 
-smokerAlcoholNprevistReg <- lm(birthweight ~ 1 + smoker + alcohol + nprevist, data = birthweight_smoking)
+smokerAlcoholNprevistReg <- lm(birthweight ~ smoker + alcohol + nprevist, data = birthweight_smoking)
 summary ( smokerAlcoholNprevistReg )
 smokerAlcoholNprevistReg$coefficients["(Intercept)"]+smokerAlcoholNprevistReg$coefficients["smoker"]+8*smokerAlcoholNprevistReg$coefficients["nprevist"]
 
@@ -18,6 +18,7 @@ library(lmtest)
 
 coeftest (smokerAlcoholNprevistReg , vcov = vcovHC (smokerAlcoholNprevistReg , type = "HC1"))
 
-Y <- resid(lm(birthweight~alcohol+nprevist, birthweight_smoking))
-X <- resid(lm(smoker~alcohol+nprevist, birthweight_smoking))
-summary(lm(Y~X))
+Y <- lm(smoker~alcohol+nprevist, birthweight_smoking)$residuals
+X <- lm(birthweight~alcohol+nprevist, birthweight_smoking)$residuals
+resids <- lm(X~Y)
+summary(resids)
